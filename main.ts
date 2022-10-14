f3936395daea4b52651158217ac267955bf7caad
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2022-10-14 20:50:48 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,9 @@
 //% color="#AA278D" weight=0 icon=\uf49e
 namespace Packing {
 
+    //% block="create new heap"
+    //% blockId="packing_create"
+    //% weight="100"
     export function create_data_heap() {
         let obj = {}
         return obj
