765b86a8a2d7f40c5817c86f5c0110a9437bcb56
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2022-10-15 12:03:22 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
 //% blockNamespace=Packing 
 
 class Heap {
-    data: object
+    data: any
     constructor() {
         this.data = {}
+    }
+
+    add(name: string, data: any) {
+        if (!name) {return}
+        this.data[name] = data
     }
 }
 
@@ -11,10 +16,17 @@ class Heap {
 //% icon="\uf187"
 namespace Packing {
     //% blockId=packing_create
-    //% color="#A37C4B"
     //% block="create new heap"
     //% blockSetVariable=data
     export function create_heap(): Heap {
         return new Heap()
+    }
+
+    //% blockId=packing_add_item
+    //% block="add property $name = $item to $heap=variables_get(data)"
+    //% name.defl="name"
+    export function add_item(heap: Heap, name: string, item: any) {
+        if (!name || !item) {return} 
+        heap.add(name, item)
     }
 }
