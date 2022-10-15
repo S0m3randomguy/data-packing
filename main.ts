8613ad3ccd3b18c2ac6a3a1f536c337bf3ee7f5b
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2022-10-15 13:21:12 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -18,16 +18,33 @@ class Heap {
         if (!this.data[name]) {return}
         delete this.data[name]
     }
+
+    get_item(name: string) {
+        let item = this.data[name]
+        if (item) {return item}
+        throw `Heap does not contain item '${name}'`
+    }
+
+    compile() {
+        let keys = Object.keys(this.data)
+        for (let i = 0; i < keys.length; i++) {
+
+        }
+    }
+
+    wipe() {
+        this.data = {}
+    }
 }
 
 //% color="#A37C4B"
 //% icon="\uf187"
-//% groups="['Creation', 'Modification', 'Encryption', 'Compile']"
+//% groups="['Create', 'Modify', 'Encrypt', 'Compile']"
 namespace Packing {
     //% blockId=packing_create
     //% block="create new heap"
     //% blockSetVariable=data
-    //% group="Creation"
+    //% group="Create"
     export function create_heap(): Heap {
         return new Heap()
     }
@@ -35,7 +52,7 @@ namespace Packing {
     //% blockId=packing_add_item
     //% block="add property $name = $item to $heap=variables_get(data)"
     //% name.defl="name"
-    //% group="Modification"
+    //% group="Modify"
     export function add_item(heap: Heap, name: string, item: any) {
         if (!name || !item) {return} 
         heap.add(name, item)
@@ -44,9 +61,24 @@ namespace Packing {
     //% blockId=packing_remove_item
     //% block="remove property $name from $heap=variables_get(data)"
     //% name.defl="name"
-    //% group="Modification"
+    //% group="Modify"
     export function remove_item(heap: Heap, name: string) {
         if (!name) {return}
         heap.remove(name)
+    }
+
+    //% blockId=packing_get_item
+    //% block="get property from $heap=variables_get(data)"
+    //% name.defl="name"
+    //% group="Modify"
+    export function get_item(heap: Heap, name: string): any {
+        return heap.get_item(name)
+    }
+
+    //% blockId=packing_compile
+    //% block="compile $heap=variables_get(data) to device storage"
+    //% group="Compile"
+    export function compile(heap: Heap) {
+        
     }
 }
