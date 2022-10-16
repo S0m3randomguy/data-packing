8589ecec846f3248669d659faa86e870e5612f83
Sarge <63020240+S0m3randomguy@users.noreply.github.com>
2022-10-16 11:59:13 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,40 @@
-//% blockNamespace=Packing 
+//% blockNamespace=Packing
+
+enum Target {
+    //% block="Device storage"
+    DeviceStorage,
+    Console
+}
+
+const MAIN_SEPARATOR  = "&"
+const ARRAY_SEPARATOR = "$"
+
+function get_type(item: any, name: string) {
+    let item_type = typeof (item)
+    if (item_type != "object") {
+        return typeof(item)
+    }
+    if (Array.isArray(item)) {
+        return "array"
+    }
+    throw `Cannot compile object '${name}' of type ${item_type}`
+}
+
+function to_string(item: any, _type: string) {
+    if (_type == "string") {
+        return item
+    } else if (_type == "number") {
+        return item.toString()
+    } else if (_type == "boolean") {
+        return item ? "true" : "false"
+    }
+    let array_string = ""
+    for (let i = 0; i < item.length; i++) {
+        array_string += item[i]
+        if (i < item.length - 1) {array_string += ARRAY_SEPARATOR}
+    }
+    return array_string
+}
 
 class Heap {
     data: any
@@ -23,10 +59,19 @@ class Heap {
         throw `Heap does not contain item '${name}'`
     }
 
-    compile() {
+    compile(target: Target, name: string) {
         let keys = Object.keys(this.data)
+        let compilation = ""
+        let _type
+        let item
         for (let i = 0; i < keys.length; i++) {
-
+            item = this.data[keys[i]]
+            _type = get_type(item, keys[i])
+            compilation += to_string(item, _type)
+            if (i < keys.length - 1) {compilation += MAIN_SEPARATOR}
+        }
+        if (target == Target.DeviceStorage) {
+            blockSettings.writeString(name, compilation)
         }
     }
 
@@ -81,9 +126,11 @@ namespace Packing {
     }
 
     //% blockId=packing_compile
-    //% block="compile $heap=variables_get(data) to device storage"
+    //% block="compile $heap=variables_get(data) to $target as $name"
+    //% name.defl="name"
     //% group="Compile"
-    export function compile(heap: Heap) {
-        
+    //% inlineInputMode=inline
+    export function compile(heap: Heap, target: Target, name: string) {
+        heap.compile(target, name)
     }
 }
